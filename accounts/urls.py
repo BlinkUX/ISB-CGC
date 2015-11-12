eb2aa4e20db43d360fd9544e29b748682c76a160
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-11-11 17:28:50 -0800

    Fix logout issues
    Fix #19. Throws because of ISB specific code. We'll need to figure out a strategy that works for both of us.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, url, include
 from allauth.socialaccount.providers.google import urls as google_urls, views as google_views
+from allauth.account import views as account_views
 
 from . import views
 
 
 urlpatterns = patterns('',
     url(r'^', include(google_urls)),
-    # url(r'^logout', account_views.logout, name='account_logout'),
-    url(r'^logout', views.extended_logout_view, name='account_logout'),
+    url(r'^logout', account_views.logout, name='account_logout'),
+    # url(r'^logout', views.extended_logout_view, name='account_logout'),
     url(r'^login/$', google_views.oauth2_login, name='account_login'),
     url(r'^nih_login/$', views.nih_login, name='nih_login'),
     url(r'^unlink_accounts/', views.unlink_accounts, name='unlink_accounts')
